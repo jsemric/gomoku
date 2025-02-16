c9e23ca9b1202c0a1829c2f1b8ccf6bb92a9f66f
Jakub Semric <jakubsemric@gmail.com>
2025-02-16 18:56:53 +0100

    Remove python implementation

--- server/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-from Cython.Build import cythonize
-
-setup(
-    ext_modules=cythonize(
-        ["gomoku/utils.pyx"],
-        compiler_directives={"language_level": "3"},
-    )
-)
